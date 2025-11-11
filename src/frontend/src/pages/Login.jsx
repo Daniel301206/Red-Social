0f313534d3bab9e1c5e2e518269c074d0adf1eb7
Magenta Paredes <paredesponcemagenta@gmail.com>
2025-11-11 08:18:53 -0500

    Fix: Nombre correcto de propiedad ClassName

--- src/frontend/src/pages/Login.jsx
+++ src/frontend/src/pages/Login.jsx
@@ -44,8 +44,8 @@ function Login() {
         <div className="flex justify-center items-center w-auto h-auto  m-15 p-5 ">
             <img src={usuario} className="w-40 h-40"/>
         </div>
-        <h1 className="text-4xl font-bold Italic text-center">Iniciar Sesion</h1>
-        <form onSubmit={handleSubmit} className="space-y-6">
+        <h1 className="text-3xl font-bold Italic text-center">Iniciar Sesion</h1>
+        <form onSubmit={handleSubmit} className="space-y-6 pb-5">
           <div>
             <label
               htmlFor="email"
@@ -53,17 +53,16 @@ function Login() {
             >
               Correo Electronico
             </label>
-            <div class="relative mt-2 rounded shadow-sm ">
+            <div className="relative mt-2 rounded shadow-sm ">
               <input
               id="email"
               type="email"
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-              class="border border-grey-200 rounded py-2 pr-2 pl-9 w-full focus:border-gray-300 focus:autline-none focus:shadow-md"
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 border border-grey-200 rounded py-2 pr-2 pl-9 w-full focus:border-gray-300 focus:autline-none focus:shadow-md"
               />
-              <div class="absolute inset-y-0 left-0 flex items-center m-3">
+              <div className="absolute inset-y-0 left-0 flex items-center m-3">
                <img src={mensaje} />
               </div>
             </div>
@@ -77,42 +76,34 @@ function Login() {
             >
               Contraseña
             </label>
-            <div class="relative mt-2 rounded shadow-sm ">
+            <div className="relative mt-2 rounded shadow-sm ">
               <input
               id="password"
               type="password"
               required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-
-              class="border border-grey-200 rounded py-2 pr-2 pl-9 w-full focus:border-gray-300 focus:autline-none focus:shadow-md"
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 border border-grey-200 rounded py-2 pr-2 pl-9 w-full focus:border-gray-300 focus:autline-none focus:shadow-md"
               />
-              <div class="absolute inset-y-0 left-0 flex items-center m-3">
+              <div className="absolute inset-y-0 left-0 flex items-center m-3">
                 <img src={clave}  />
               </div>  
             </div>
             
           </div>
           {error && <p className="text-sm text-center text-red-500"></p>}
-          <div>
             <button
               type="submit"
               className="w-full px-4 py-2 text-white font-medium bg-blue-500 border border-transparent rounded-md shadow-md hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 "
             >
               Iniciar Sesion
             </button>
-          </div>
         </form>
-        <div className="flex items-center">
-          <div className="flex-grow border-t border-gray-300"></div>
-          <span className="mx-4 text-sm font-medium text-gray-500">O</span>
-          <div className="flex-grow border-t border-gray-300"></div>
-        </div>
-        <p className="text-sm text-center text-gray-600">
+
+        <p className="text-sm text-center text-gray-600 pb-4">
           No tienes una cuenta?
         </p>
-        <div class="flex justify-center items-center align-items-center">
+        <div className="flex justify-center items-center align-items-center">
           <Link
           to="/register"
           className="font-medium text-blue-600 hover:text-orange-500"
