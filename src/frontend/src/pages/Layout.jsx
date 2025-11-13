2e263ca3e4ba847b986a712ed4ca2ab2af8c9f35
Daniel <daniel3012riberr@gmail.com>
2025-11-13 03:04:10 -0500

    feat:actualizando el sistema

--- src/frontend/src/pages/Layout.jsx
+++ src/frontend/src/pages/Layout.jsx
@@ -27,12 +27,15 @@ const Layout = ({ children }) => {
           </div>
 
           {/* Buscador */}
-          <div className="hidden md:flex items-center bg-gray-800 rounded-full px-3 py-1">
-            <img src={lupa} alt="Buscar" className="w-5 h-5 mr-2" />
+          <div className="md:flex items-center bg-gray-800 rounded-full px-3 py-1">
+            <button className="hidden md:flex  text-black px-3 py-2 rounded">
+            <img src={lupa} alt="Buscar" className="w-6 h-6 mr-2" />
+            </button>
             <input
               type="text"
               placeholder="Buscar..."
-              className="bg-gray-800 text-gray-200 placeholder-gray-400 outline-none"
+              className="bg-gray-800 text-gray-100 placeholder-gray-00
+              outline-none"
             />
           </div>
 
