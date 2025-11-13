63cefe568f1256dd2c1c8ba3f75ccc955746c307
Daniel <daniel3012riberr@gmail.com>
2025-11-12 23:16:47 -0500

    feat:actualizando el sistema

--- src/frontend/src/pages/Notificaciones.jsx
+++ src/frontend/src/pages/Notificaciones.jsx
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 function Notificaciones() {
   return (
     <Layout>
-      <div className="text-center text-yellow-400 py-10">
+      <div className="text-center text-green-900 py-10">
         🔔 <strong>Notificaciones</strong>
         <p className="text-gray-300 mt-2">Aquí verás tus notificaciones recientes.</p>
       </div>
